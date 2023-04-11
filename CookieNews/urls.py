5378f38a1794003a9c5f7a7c52c6a21b4d964420
Kim Kyungjin <grace8362@naver.com>
2023-04-11 11:34:14 +0900

    add board url

--- CookieNews/urls.py
+++ CookieNews/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from posting import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
+    path('save-posting', views.save_posting, name='save-posting'),
 ]
--- posting/views.py
+++ posting/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def home(request):
+    user = request.user.is_authenticated # 로그인 여부
+    redirect('/login')
+    # if user:
+    #     return redirect('/save_posting')
+    # else:
+    #     return redirect('/login')
+
+def save_posting(request):
+    if request.method == 'GET':
+        user = request.user.is_authenticated
+        if user:
+
+            return render(request, '../templates/posting/save_posting.html')
+        else:
+            return redirect('/login')
