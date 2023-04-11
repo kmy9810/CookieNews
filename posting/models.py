5c7b161ea45dc4d4017555f6841b58fadfe2b277
Kim Kyungjin <grace8362@naver.com>
2023-04-11 14:23:06 +0900

    edit board form

--- posting/models.py
+++ posting/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class PostingModel(models.Model):
+    class Meta:
+        db_table = "my_board"
+
+    posting_id = models.IntegerField(max_length=10)
+    posting_author = models.IntegerField(max_length=10)   # UserModel을 참조하는 외래키
+    posting_category = models.CharField(max_length=10)
+    posting_title = models.CharField(max_length=20)
+    posting_content = models.TextField()
+    posting_created = models.DateTimeField(auto_now_add=True)
