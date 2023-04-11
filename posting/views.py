0b5c081bd290c2c62c284f44bc2de78542955c9c
Kim Kyungjin <grace8362@naver.com>
2023-04-11 11:37:07 +0900

    edit board url

--- posting/views.py
+++ posting/views.py
@@ -15,9 +15,10 @@ def home(request):
 
 def save_posting(request):
     if request.method == 'GET':
-        user = request.user.is_authenticated
-        if user:
-
-            return render(request, '../templates/posting/save_posting.html')
-        else:
-            return redirect('/login')
+        return render(request, '../templates/posting/save_posting.html')
+        # user = request.user.is_authenticated
+        # if user:
+        #
+        #     return render(request, '../templates/posting/save_posting.html')
+        # else:
+        #     return redirect('/login')
