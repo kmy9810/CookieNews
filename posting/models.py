262c0e23c888da913e35d183e905bbc637264e80
Kim Kyungjin <grace8362@naver.com>
2023-04-11 15:46:19 +0900

    edit PostingModel #17

--- posting/models.py
+++ posting/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+import user.models
+
+
 # Create your models here.
 class PostingModel(models.Model):
     class Meta:
         db_table = "my_board"
 
     posting_id = models.IntegerField(max_length=10)
-    posting_author = models.ForeignKey(UserModel, on_delete = models.CASCADE)   # UserModel을 참조하는 외래키
+    posting_author = models.ForeignKey(user.models.UserModel, on_delete = models.CASCADE)   # UserModel을 참조하는 외래키
     posting_category = models.CharField(max_length=10)
     posting_title = models.CharField(max_length=20)
     posting_content = models.TextField()
